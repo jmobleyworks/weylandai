912e5baadb2a38ebb9f8c4ac194dcc1e47e7a77b
John Mobley <jmobleyworks@gmail.com>
2026-07-18 17:33:49 -0400

    Fecundity Vector: Initial Mass Deployment

---
+++ weylandaiAttractor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("/Users/johnmobley/mascom")
+from BaseAttractor import attract_legacy_work
+
+if __name__ == "__main__":
+    attract_legacy_work("weylandai")
